df98f230f74f08163210fbe0506b35118bebdbae
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-11-09 15:01:16 -0500

    Binary search for extra end

--- sudoku.rb
+++ sudoku.rb
@@ -13,7 +13,7 @@ class SudokuGame
     board = Board.from_file(filename)
     self.new(board)
   end
-
+  
   def initialize(board)
     @board = board
   end
@@ -23,13 +23,13 @@ class SudokuGame
     until pos && valid_pos?(pos)
       puts "Please enter a position on the board (e.g., '3,4')"
       print "> "
-
+  
       begin
         pos = parse_pos(gets.chomp)
       rescue
         puts "Invalid position entered (did you use a comma?)"
         puts ""
-
+  
         pos = nil
       end
     end
@@ -45,11 +45,11 @@ class SudokuGame
     end
     val
   end
-
+  
   def parse_pos(string)
     string.split(",").map { |char| Integer(char) }
   end
-
+  
   def parse_val(string)
     Integer(string)
   end
@@ -60,28 +60,28 @@ class SudokuGame
     val = get_val
     board[pos] = val
   end
-
+  
   def run
     play_turn until solved?
     board.render
     puts "Congratulations, you win!"
   end
-
+  
   def solved?
     board.solved?
   end
-
+  
   def valid_pos?(pos)
     pos.is_a?(Array) &&
       pos.length == 2 &&
       pos.all? { |x| x.between?(0, board.size - 1) }
   end
-
+  
   def valid_val?(val)
     val.is_a?(Integer) &&
       val.between?(0, 9)
   end
-
+  
   private
   attr_reader :board
 end
